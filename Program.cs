c2d58c1caee28eba2e8889dc15e1d39571a96320
MG Creations <info@mgcreations.co.za>
2020-06-21 19:06:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace DemoORAndANDLogic
+{
+    class Program
+    {
+        static void Main()
+        {
+            const int ZONE1 = 1, ZONE2 = 2;
+            const int LOWQUANTITY = 10;
+            string inputString;
+            int quantity;
+            int deliveryZone;
+
+            Console.WriteLine("Delivery is free for zone {0} or {1}", ZONE1, ZONE2);
+            Console.WriteLine("when the number of boxes is less than {0}", LOWQUANTITY);
+            Console.WriteLine("Enter delivery zone ");
+            inputString = Console.ReadLine();
+            deliveryZone = Convert.ToInt32(inputString);
+            Console.WriteLine("Enter the number of boxes in the shipment");
+            inputString = Console.ReadLine();
+            quantity = Convert.ToInt32(inputString);
+
+            if ((deliveryZone == ZONE1 || deliveryZone == ZONE2) && quantity < LOWQUANTITY)
+                Console.WriteLine("Delivery is free");
+            else
+                Console.WriteLine("A delivery charge applies");
+        }
+    }
+}
